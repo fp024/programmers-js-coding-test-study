b2f8457c8a58806636d77eed0429f3b28b2053c0
fp024 <aopenasus@naver.com>
2025-02-14 16:59:12 +0900

    문자 리스트를 문자열로 변환하기 - 181941

---
+++ src/training/exam010-181941.js
@@ -0,0 +1,13 @@
+/*
+ * 문자 리스트를 문자열로 변환하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181941
+ */
+function solution(arr) {
+  let answer = '';
+
+  answer = arr.join('');
+
+  return answer;
+}
+// 단순 실행 테스트
+console.log(solution(['a', 'b', 'c']) === 'abc');
