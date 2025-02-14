92967d1265a302f9287c97acedae95f47a9bb553
fp024 <aopenasus@naver.com>
2025-02-14 18:25:03 +0900

    flag에 따라 다른 값 반환하기 - 181933

---
+++ src/training/exam016-181933.js
@@ -0,0 +1,20 @@
+/*
+ * flag에 따라 다른 값 반환하기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181933
+ */
+function solution(a, b, flag) {
+  let answer = flag ? a + b : a - b;
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+if (solution(-4, 7, true) !== 3) {
+  throw Error('Test Case 01 Failed.');
+}
+
+if (solution(-4, 7, false) !== -11) {
+  throw Error('Test Case 02 Failed.');
+}
+
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
