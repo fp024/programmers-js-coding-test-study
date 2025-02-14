f6cf0a4f03cb7f1d88de24edeac77a49e1bdeafe
fp024 <aopenasus@naver.com>
2025-02-14 17:32:01 +0900

    두 수의 연산값 비교하기 - 181938

---
+++ src/training/exam013-181938.js
@@ -0,0 +1,26 @@
+/*
+ * 두 수의 연산값 비교하기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181938
+ */
+function solution(a, b) {
+  let answer = 0;
+
+  let first = Number(`${a}${b}`);
+  let second = 2 * a * b;
+
+  answer = first >= second ? first : second;
+
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+if (solution(2, 91) !== 364) {
+  throw Error('Test Case 01 Failed.');
+}
+
+if (solution(91, 2) !== 912) {
+  throw Error('Test Case 02 Failed.');
+}
+
+// === 다른 사람 풀이 확인 이후 의견===
+// 별로 특별한 점은 없었다. 😅
