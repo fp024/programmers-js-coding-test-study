eaf91e5a2ed0dc86d185bc42b5de76c66d461097
fp024 <aopenasus@naver.com>
2025-02-19 18:16:18 +0900

    배열 만들기 2 - 181921
    정규식 사용하니 아주 간단하게 풀렸다.

--- src/training/exam028-181921.js
+++ src/training/exam028-181921.js
@@ -4,14 +4,23 @@
  */
 function solution(l, r) {
   let answer = [];
-  return answer;
+
+  for (let i = l; i <= r; i++) {
+    if (i % 5 == 0 && /^[05]+$/.test('' + i)) {
+      answer.push(i);
+    }
+  }
+  return answer.length === 0 ? [-1] : answer;
 }
 
 // === 단순 실행 테스트 ===
 // NOSONAR
+console.log(solution(10, 20));
+console.log(solution(10, 555));
 
 export default solution;
 // === 문제 읽고 첫 느낌 ===
 // 뭔가 바로 생각이 안나는데... 😂
+// 이후에 그냥 정규식을 써봤는데... 코드 자체는 아주 간단하게 끝났다.
 // === 다른 사람 풀이 확인 이후 의견 ===
 // ...
