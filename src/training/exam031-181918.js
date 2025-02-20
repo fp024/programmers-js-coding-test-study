9d9ad01d9bc2d4eda28de17347b52ce4905613c0
fp024 <aopenasus@naver.com>
2025-02-20 10:13:22 +0900

    배열 만들기 4 - 181918

---
+++ src/training/exam031-181918.js
@@ -0,0 +1,29 @@
+/*
+ * 배열 만들기 4
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181918
+ */
+function solution(arr) {
+  let stk = [];
+
+  let i = 0;
+  while (i < arr.length) {
+    if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      i++;
+    } else if (stk[stk.length - 1] >= arr[i]) {
+      stk.splice(stk.length - 1);
+    }
+  }
+
+  return stk;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution([1, 4, 2, 5, 3]));
+
+export default solution;
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// 배열의 pop() 메서드가 배열을 수정하지 않는줄 알았다. 😅
---
+++ src/training/exam031-181918.test.js
@@ -0,0 +1,7 @@
+import solution from './exam031-181918.js';
+
+describe('solution', () => {
+  it('TestCase 01', () => {
+    expect(solution([1, 4, 2, 5, 3])).toEqual([1, 2, 3]);
+  });
+});
