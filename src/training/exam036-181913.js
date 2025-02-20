37ea0d98a2987cff46249489544bd22914ed365c
fp024 <aopenasus@naver.com>
2025-02-20 12:58:18 +0900

    문자열 여러 번 뒤집기 - 181913

---
+++ src/training/exam036-181913.js
@@ -0,0 +1,34 @@
+/*
+ * 문자열 여러 번 뒤집기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181913
+ */
+function solution(my_string, queries) {
+  let answer = '';
+
+  const myStringArray = [...my_string];
+
+  queries.forEach(([s, e]) => {
+    let target = myStringArray.slice(s, e + 1).reverse();
+    myStringArray.splice(s, target.length, ...target);
+  });
+
+  answer = myStringArray.join('');
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(
+  solution('rermgorpsam', [
+    [2, 3],
+    [0, 7],
+    [5, 9],
+    [6, 10],
+  ])
+);
+
+export default solution;
+// === 문제 읽고 첫 느낌 ===
+// 💡 splice에 배열을 아이템을 추가할 때.. 구조분해해서 넣어야함.
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
