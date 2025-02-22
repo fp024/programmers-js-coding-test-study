2b6014b4bb89a45837b62c5e91ff9e5285b89c7c
fp024 <aopenasus@naver.com>
2025-02-22 10:43:18 +0900

    문자열 앞의 n글자 - 181907

---
+++ src/ts/training/exam006-181907.ts
@@ -0,0 +1,22 @@
+/*
+ * 문자열 앞의 n글자
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181907
+ */
+function solution(my_string: string, n: number) {
+  let answer = '';
+
+  answer = my_string.slice(0, n);
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('ProgrammerS123', 11));
+console.log(solution('He110W0r1d', 5));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
