75f91b576bfd4bb392cd1ab9ad95509fcb8cab5e
fp024 <aopenasus@naver.com>
2025-02-23 11:29:11 +0900

    배열 조각하기 - 181893

---
+++ src/ts/training/exam020-181893.ts
@@ -0,0 +1,30 @@
+/*
+ * 배열 조각하기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181893
+ */
+function solution(arr: number[], query: number[]) {
+  let answer: number[];
+
+  query.forEach((q, i) => {
+    if (i % 2 == 0) {
+      arr.splice(q + 1);
+    } else {
+      arr.splice(0, q);
+    }
+  });
+
+  answer = arr;
+
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution([0, 1, 2, 3, 4, 5], [4, 1, 2]));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
