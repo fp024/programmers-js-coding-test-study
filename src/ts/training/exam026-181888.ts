27aef7c0004075076895fdf4fda320dce0e41a9b
fp024 <aopenasus@naver.com>
2025-02-24 12:33:56 +0900

    n개 간격의 원소들 - 181888

---
+++ src/ts/training/exam026-181888.ts
@@ -0,0 +1,26 @@
+/*
+ * n개 간격의 원소들
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181888
+ */
+function solution(num_list: number[], n: number) {
+  let answer: number[] = [];
+
+  for (let i = 0; i < num_list.length; i += n) {
+    answer.push(num_list[i]);
+  }
+
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution([4, 2, 6, 1, 7, 6], 2));
+console.log(solution([4, 2, 6, 1, 7, 6], 4));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
