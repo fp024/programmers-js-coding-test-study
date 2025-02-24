65a331ca4e1022a8ad049d92bec39f7d8a917fc0
fp024 <aopenasus@naver.com>
2025-02-24 20:38:30 +0900

    그림 확대 - 181836

---
+++ src/ts/training/exam033-181836.ts
@@ -0,0 +1,53 @@
+/*
+ * 그림 확대
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181836
+ */
+function solution(picture: string[], k: number) {
+  let answer: string[] = [];
+
+  for (let p of picture) {
+    // 가로 늘리기
+    const expendWidth = [...p].map((c) => c.repeat(k)).join('');
+    // 세로 늘리기
+    for (let i = 0; i < k; i++) {
+      answer.push(expendWidth);
+    }
+  }
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(
+  solution(
+    [
+      '.xx...xx.', //
+      'x..x.x..x',
+      'x...x...x',
+      '.x.....x.',
+      '..x...x..',
+      '...x.x...',
+      '....x....',
+    ],
+    2
+  )
+);
+
+console.log(
+  solution(
+    [
+      'x.x', //
+      '.x.',
+      'x.x',
+    ],
+    3
+  )
+);
+
+// === 문제 읽고 첫 느낌 ===
+// 세로늘리기가 단순 복사라 그다지 어렵지는 않았다. 😅
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
