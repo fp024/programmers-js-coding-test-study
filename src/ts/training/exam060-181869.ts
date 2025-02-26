a77359f9ea9e39fd8023233234f3cac19bf7c4b7
fp024 <aopenasus@naver.com>
2025-02-26 17:25:40 +0900

    공백으로 구분하기 1 - 181869

---
+++ src/ts/training/exam060-181869.ts
@@ -0,0 +1,21 @@
+/*
+ * 공백으로 구분하기 1
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181869
+ */
+function solution(my_string: string) {
+  return my_string.split(' ');
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('i love you'));
+
+console.log(solution('programmers'));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
