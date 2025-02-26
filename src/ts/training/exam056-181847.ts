d572ec175d1b61400812e012508b317d0fd42e3b
fp024 <aopenasus@naver.com>
2025-02-26 17:07:17 +0900

    0 떼기 - 181847

---
+++ src/ts/training/exam056-181847.ts
@@ -0,0 +1,22 @@
+/*
+ * 0 떼기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181847
+ */
+function solution(n_str: string) {
+  let answer = n_str.replace(/^0+/, '');
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('0010'));
+
+console.log(solution('854020'));
+
+// === 문제 읽고 첫 느낌 ===
+// 나도 boolean결과에 +붙여서 결과를 바꿔봤다. 😅
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
