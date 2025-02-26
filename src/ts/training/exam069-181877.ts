7a1be604cc12b236da75249ab81162ea423d2e54
fp024 <aopenasus@naver.com>
2025-02-26 19:21:34 +0900

    대문자로 바꾸기 - 181877

---
+++ src/ts/training/exam069-181877.ts
@@ -0,0 +1,21 @@
+/*
+ * 대문자로 바꾸기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181877
+ */
+function solution(myString: string) {
+  return myString.toUpperCase();
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('aBcDeFg'));
+
+console.log(solution('AAA'));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
