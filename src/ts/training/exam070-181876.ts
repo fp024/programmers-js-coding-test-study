2b8ae1cdccb6609b787b8f96997c65a63f824b38
fp024 <aopenasus@naver.com>
2025-02-26 19:23:11 +0900

    소문자로 바꾸기 - 181876

---
+++ src/ts/training/exam070-181876.ts
@@ -0,0 +1,21 @@
+/*
+ * 소문자로 바꾸기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181876
+ */
+function solution(myString: string) {
+  return myString.toLowerCase();
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('aBcDeFg'));
+
+console.log(solution('aaa'));
+
+// === 문제 읽고 첫 느낌 ===
+// 문제가 너무 쉬워져서 이제는 프로젝트에 포함하지 말고... 그냥 웹에서만 풀어도 되겠다. 😅
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
