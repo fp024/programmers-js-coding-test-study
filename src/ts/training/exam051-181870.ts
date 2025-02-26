0de33014c2d1abd811cbbf12e01414720ae5390f
fp024 <aopenasus@naver.com>
2025-02-26 16:36:33 +0900

    ad 제거하기 - 181870

---
+++ src/ts/training/exam051-181870.ts
@@ -0,0 +1,23 @@
+/*
+ * ad 제거하기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/181870
+ */
+function solution(strArr: string[]) {
+  let answer: string[] = strArr.filter((s) => s.indexOf('ad') < 0);
+
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution(['and', 'notad', 'abcd']));
+
+console.log(solution(['there', 'are', 'no', 'a', 'ds']));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 힌트 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+export default solution;
