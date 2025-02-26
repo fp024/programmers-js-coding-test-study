680150ec827e10f63805855431354f68bb6f59b1
fp024 <aopenasus@naver.com>
2025-02-26 18:23:18 +0900

    eval()로 처리한게 좀 그래서.. 다시 풀었음 😅 - 181865

--- src/ts/training/exam064-181865.ts
+++ src/ts/training/exam064-181865.ts
@@ -3,7 +3,15 @@
  *   https://school.programmers.co.kr/learn/courses/30/lessons/181865
  */
 function solution(binomial: string) {
-  return eval(binomial);
+  const [a, op, b] = binomial.split(' ');
+
+  const operation: { [key: string]: (a: number, b: number) => number } = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+  };
+
+  return operation[op](Number(a), Number(b));
 }
 
 // === 단순 실행 테스트 ===
@@ -15,7 +23,8 @@ console.log(solution('0 - 7777'));
 console.log(solution('40000 * 40000'));
 
 // === 문제 읽고 첫 느낌 ===
-//   eval()쓰란 말은 아닐 것 같은데.. 그냥 써봤다. 😂
+//   eval()쓰란 말은 아닐 것 같은데.. 그냥 써봤다. 😂 - 다시 풀었다.
+//   💡 TS라서 key에 대해서도 타입 정확하게 명시를 해야했다.
 // === 힌트 ===
 // ...
 // === 다른 사람 풀이 확인 이후 의견 ===
