f58f66561c052df02c6e64a3c4031cf76fa49ec0
fp024 <aopenasus@naver.com>
2025-04-08 16:05:35 +0900

    정수 내림차순으로 배치하기 - 12933

---
+++ src/ts/lv_1/exam005-12933.ts
@@ -0,0 +1,23 @@
+/*
+ * 정수 내림차순으로 배치하기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12933
+ */
+function solution(n: number) {
+  let answer = n
+    .toString()
+    .split('')
+    .sort((a, b) => parseInt(b) - parseInt(a))
+    .join('');
+
+  return +answer;
+}
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution(118372));
+
+// === 문제 읽고 첫 느낌 ===
+//   이상하게 이 문제는 반환 값 타입 검사를 한다. answer 앞에 + 붙여서 number로 형변환 했음.
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
