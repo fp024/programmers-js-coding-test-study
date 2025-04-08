8df29d77512abf936d9d429f3315db0528bb0f26
fp024 <aopenasus@naver.com>
2025-04-08 15:51:08 +0900

    문자열 내 p와 y의 개수 - 12916

---
+++ src/ts/lv_1/exam004-12916.ts
@@ -0,0 +1,31 @@
+/*
+ * 문자열 내 p와 y의 개수
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12916
+ */
+function solution(s: string) {
+  let pCount = 0;
+  let yCount = 0;
+
+  s.split('').forEach((c) => {
+    if (c.toLowerCase() === 'p') {
+      pCount++;
+    } else if (c.toLowerCase() === 'y') {
+      yCount++;
+    }
+  });
+
+  let answer = pCount === yCount;
+
+  return answer;
+}
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('pPoooyY'));
+console.log(solution('Pyy'));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+//   첫번째 풀이가 뭔가 새롭다.. 검사할 문자열로 split 해서 나뉜 요소 수를 비교한 것 같은데...
+//
+export default solution;
