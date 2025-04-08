88055a1ea08bb4ab661a1946f2d2f7d1d3d6cdcd
fp024 <aopenasus@naver.com>
2025-04-08 15:27:54 +0900

    나머지가 1이 되는 수 찾기 - 87389

---
+++ src/ts/lv_1/exam002-87389.ts
@@ -0,0 +1,27 @@
+/*
+ * 나머지가 1이 되는 수 찾기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/87389
+ */
+function solution(n: number) {
+  let i = 1;
+  while (i < n) {
+    if (n % i === 1) {
+      break;
+    }
+    i++;
+  }
+
+  const answer = i;
+  return answer;
+}
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution(10));
+console.log(solution(12));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
