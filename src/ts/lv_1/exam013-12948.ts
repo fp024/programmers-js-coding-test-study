ad2852e2eacd8ece3b8e35b6a361a779edd54e6c
fp024 <aopenasus@naver.com>
2025-04-22 03:03:54 +0900

    핸드폰 번호 가리기 - 12948

---
+++ src/ts/lv_1/exam013-12948.ts
@@ -0,0 +1,23 @@
+/*
+ * 핸드폰 번호 가리기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12948
+ */
+function solution(phone_number: string) {
+  let answer;
+
+  const maskingCount = phone_number.length - 4;
+  answer = '*'.repeat(maskingCount) + phone_number.slice(maskingCount);
+
+  return answer;
+}
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('01033334444'));
+console.log(solution('027778888'));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
