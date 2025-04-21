99cdc51caaf57af236bdb50b0af04b3d3663a286
fp024 <aopenasus@naver.com>
2025-04-22 02:29:50 +0900

    서울에서 김서방 찾기 - 12919

---
+++ src/ts/lv_1/exam011-12919.ts
@@ -0,0 +1,19 @@
+/*
+ * 서울에서 김서방 찾기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12919
+ */
+function solution(seoul: string[]) {
+  const idx = seoul.findIndex((v, i) => v === 'Kim');
+
+  return `김서방은 ${idx}에 있다`;
+}
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution(['Jane', 'Kim']));
+
+// === 문제 읽고 첫 느낌 ===
+// ...
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
