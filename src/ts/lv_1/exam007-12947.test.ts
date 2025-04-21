48587fb1174d546aa7121b10c5c000a816d51a9b
fp024 <aopenasus@naver.com>
2025-04-22 01:20:34 +0900

    JetBrains AI에게 테스트 코드 검토 부탁
    내가 잘모르고 it를 썼었는데,
    BDD(it)이냐, TDD(test)이냐의 스타일차이라고 함,
    
    그냥 이제부터는 내게 익숙한 test 쓰는게 낫겠다.

--- src/ts/lv_1/exam007-12947.test.ts
+++ src/ts/lv_1/exam007-12947.test.ts
@@ -1,7 +1,7 @@
 import solution from './exam007-12947';
 
-describe('solution', () => {
-  it('TestCase 01', () => {
+describe('하샤드 수 판별기', () => {
+  test('기본 테스트 케이스 검증', () => {
     expect(solution(10)) //
       .toBeTruthy();
     expect(solution(12)) //
@@ -10,5 +10,16 @@ describe('solution', () => {
       .toBeFalsy();
     expect(solution(13)) //
       .toBeFalsy();
+  });
+
+  test('경계값 검증', () => {
+    expect(solution(1)).toBeTruthy();
+    expect(solution(9)).toBeTruthy();
+    expect(solution(1000)).toBeTruthy();
+  });
+
+  test('특수 케이스 검증', () => {
+    expect(solution(18)).toBeTruthy(); // 1+8=9, 18은 9로 나누어짐
+    expect(solution(100)).toBeTruthy(); // 1+0+0=1, 100은 1로 나누어짐
   });
 });
