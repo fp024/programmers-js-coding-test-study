4bc638566b3eb7b8566ffa835af41bfd65e818ed
fp024 <aopenasus@naver.com>
2025-04-25 09:50:37 +0900

    문자열 다루기 기본 - 12918

---
+++ src/ts/lv_1/exam021-12918.test.ts
@@ -0,0 +1,18 @@
+import solution from './exam021-12918';
+
+describe('문자열 다루기 기본', () => {
+  test('기본 테스트 케이스 검증', () => {
+    expect(solution('a234')) //
+      .toBeFalsy();
+    expect(solution('1234')) //
+      .toBeTruthy();
+  });
+  test('길이가 4 또는 8이여야한다.', () => {
+    expect(solution('123')) //
+      .toBeFalsy();
+    expect(solution('12345')) //
+      .toBeFalsy();
+    expect(solution('123456789')) //
+      .toBeFalsy();
+  });
+});
---
+++ src/ts/lv_1/exam021-12918.ts
@@ -0,0 +1,25 @@
+/*
+ * 문자열 다루기 기본
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12918
+ */
+function solution(s: string) {
+  if (s.length !== 4 && s.length !== 6) {
+    return false;
+  }
+
+  return s.split('').findIndex((s) => Number.isNaN(Number(s))) < 0;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('a1234'));
+console.log(solution('1234'));
+
+// === 문제 읽고 첫 느낌 ===
+//   갑자기... 문자열을 숫자로 어떻게 확인하지? 해깔렸는데,
+//   그냥 Number에다 넣어서, 이게 숫자인지 숫자가 아닌지 확인해주면 되었다.
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+//   length 조건을 체크해야하는 줄 몰랐다. 😂
+//
+export default solution;
