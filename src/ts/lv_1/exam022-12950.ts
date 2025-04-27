aaa642089e704319f7b808ddc1f05d4100ba81cd
fp024 <aopenasus@naver.com>
2025-04-28 02:30:26 +0900

    행렬의 덧셈 - 12950

---
+++ src/ts/lv_1/exam022-12950.ts
@@ -0,0 +1,37 @@
+/*
+ * 행렬의 덧셈
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12950
+ */
+function solution(arr1: number[][], arr2: number[][]) {
+  return arr1.map(
+    (
+      row,
+      i //
+    ) => row.map((val, j) => val + arr2[i][j])
+  );
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(
+  solution(
+    [
+      [1, 2],
+      [2, 3],
+    ],
+    [
+      [3, 4],
+      [5, 6],
+    ]
+  )
+);
+
+console.log(solution([[1], [2]], [[3], [4]]));
+
+// === 문제 읽고 첫 느낌 ===
+//   행렬은 문제가 복잡하지 않아도 등장하면 해깔린 뭔가가 있다. 😂
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
