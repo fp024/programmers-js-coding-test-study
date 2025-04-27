208cd61ae64c6436351ddc696632ecf803ff4155
fp024 <aopenasus@naver.com>
2025-04-28 05:26:04 +0900

    (완료) 같은 숫자는 싫어 - 12906
    처음 초기화할 때.. push 한번 더한 것을 문제 삼은 것 같다.
    
    인라인으로 초기화해주니 통과함..😅

--- src/ts/lv_1/exam023-12906.ts
+++ src/ts/lv_1/exam023-12906.ts
@@ -3,19 +3,14 @@
  *   https://school.programmers.co.kr/learn/courses/30/lessons/12906
  */
 function solution(arr: number[]) {
-  let answer: number[] = [];
-
-  // answer.push(arr[0]);
-  let numStr = '' + arr[0];
+  let answer: number[] = [arr[0]];
 
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] != arr[i]) {
-      // answer.push(arr[i]);
-      numStr += arr[i];
+      answer.push(arr[i]);
     }
   }
 
-  answer = numStr.split('').map((v) => Number(v));
   return answer;
 }
 
@@ -26,9 +21,9 @@ console.log(solution([1, 1, 3, 3, 0, 1, 1]));
 console.log(solution([4, 4, 4, 3, 3]));
 
 // === 문제 읽고 첫 느낌 ===
-// ...
+//   처음 생각한게 맞았나보다.. 첫번째 요소 초기화부분을 push()하지않고 인라인으로 바꾸면 통과함.
 //
 // === 다른 사람 풀이 확인 이후 의견 ===
-// ...
+//   첫번째 풀이에서 filter로 쉽게 해결 하셨다. 👍👍
 //
 export default solution;
