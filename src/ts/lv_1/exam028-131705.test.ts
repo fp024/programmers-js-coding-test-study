47f7a74adefc965e96b1fdf577c8a6d4a0816993
fp024 <aopenasus@naver.com>
2025-05-07 17:15:14 +0900

    삼총사 - AI 개선 제안 - 131705

--- src/ts/lv_1/exam028-131705.test.ts
+++ src/ts/lv_1/exam028-131705.test.ts
@@ -1,4 +1,5 @@
 import solution from './exam028-131705';
+import solution_a from './exam028-131705-a';
 
 describe('삼총사', () => {
   test('기본 테스트 케이스 검증', () => {
@@ -7,6 +8,17 @@ describe('삼총사', () => {
     expect(solution([-3, -2, -1, 0, 1, 2, 3])) //
       .toBe(5);
     expect(solution([-1, 1, -1, 1])) //
+      .toBe(0);
+  });
+});
+
+describe('삼총사 - AI 코드 개선 제안', () => {
+  test('기본 테스트 케이스 검증', () => {
+    expect(solution_a([-2, 3, 0, 2, -5])) //
+      .toBe(2);
+    expect(solution_a([-3, -2, -1, 0, 1, 2, 3])) //
+      .toBe(5);
+    expect(solution_a([-1, 1, -1, 1])) //
       .toBe(0);
   });
 });
