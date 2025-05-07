9c63964bb1305791b1bbbda7fe71b6aaae144ba7
fp024 <aopenasus@naver.com>
2025-05-07 15:36:56 +0900

    크기가 작은 부분 문자열 - 147355

---
+++ src/ts/lv_1/exam027-147355.test.ts
@@ -0,0 +1,12 @@
+import solution from './exam027-147355';
+
+describe('크기가 작은 부분문자열', () => {
+  test('기본 테스트 케이스 검증', () => {
+    expect(solution('3141592', '271')) //
+      .toBe(2);
+    expect(solution('500220839878', '7')) //
+      .toBe(8);
+    expect(solution('10203', '15')) //
+      .toBe(3);
+  });
+});
---
+++ src/ts/lv_1/exam027-147355.ts
@@ -0,0 +1,29 @@
+/*
+ * 크기가 작은 부분 문자열
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/147355
+ */
+function solution(t: string, p: string) {
+  let answer = 0;
+
+  for (let i = 0; i <= t.length - p.length; i++) {
+    if (Number(t.slice(i, i + p.length)) <= Number(p)) {
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR
+console.log(solution('3141592', '271'));
+console.log(solution('500220839878', '7'));
+console.log(solution('10203', '15'));
+
+// === 문제 읽고 첫 느낌 ===
+//   p의 길이만큼 t를 잘 쪼개는게 중요한 것 같다.
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+//   다른 사람들하고 비슷하게 풀은 것 같다.
+//
+export default solution;
