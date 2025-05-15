ea5d9e882ba492a7c00db982ff9590f8fc2a1f8a
fp024 <aopenasus@naver.com>
2025-05-15 18:29:33 +0900

    2016년 - 12901

---
+++ src/ts/lv_1/exam044-12901.test.ts
@@ -0,0 +1,22 @@
+import solution from './exam044-12901';
+
+// cspell:disable
+const DEFAULT_TEST_CASES = [
+  {
+    a: 5,
+    b: 24,
+    expected: 'TUE',
+  },
+  {
+    a: 1,
+    b: 1,
+    expected: 'FRI',
+  },
+];
+// cspell:enable
+
+describe('2016년', () => {
+  test.each(DEFAULT_TEST_CASES)('기본 테스트 케이스 검증', ({ a, b, expected }) => {
+    expect(solution(a, b)).toEqual(expected);
+  });
+});
---
+++ src/ts/lv_1/exam044-12901.ts
@@ -0,0 +1,24 @@
+/*
+ * 2016년
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12901
+ */
+function solution(a: number, b: number) {
+  const DAY_NAMES = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+  const MONTH_DAYS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const targetDays = MONTH_DAYS.filter((_, i) => i < a - 1).reduce((acc, d) => acc + d, 0) + b - 1;
+  console.log(targetDays);
+  return DAY_NAMES[targetDays % 7];
+}
+// === 단순 실행 테스트 ===
+// NOSONAR // cspell:disable
+console.log(solution(5, 24));
+console.log(solution(1, 1));
+// cspell:enable
+//
+// === 문제 읽고 첫 느낌 ===
+// 잘 짜맞추어서 풀긴 했음.
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// 두번째 푸신 분하고 비슷하게 풀었다.
+//
+export default solution;
