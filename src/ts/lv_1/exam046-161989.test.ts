b322fe88b35a72de3f70f6e8ab074cf6e85676a9
fp024 <aopenasus@naver.com>
2025-05-18 13:31:29 +0900

    덧칠하기 - 161989

---
+++ src/ts/lv_1/exam046-161989.test.ts
@@ -0,0 +1,30 @@
+import solution from './exam046-161989';
+
+// cspell:disable
+const DEFAULT_TEST_CASES = [
+  {
+    n: 8,
+    m: 4,
+    section: [2, 3, 6],
+    expected: 2,
+  },
+  {
+    n: 5,
+    m: 4,
+    section: [1, 3],
+    expected: 1,
+  },
+  {
+    n: 4,
+    m: 1,
+    section: [1, 2, 3, 4],
+    expected: 4,
+  },
+];
+// cspell:enable
+
+describe('덧칠하기', () => {
+  test.each(DEFAULT_TEST_CASES)('기본 테스트 케이스 검증', ({ n, m, section, expected }) => {
+    expect(solution(n, m, section)).toBe(expected);
+  });
+});
---
+++ src/ts/lv_1/exam046-161989.ts
@@ -0,0 +1,44 @@
+/**
+ * 덧칠하기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/161989
+ * @param n   벽 길이(미터)
+ * @param m   롤러의 길이(미터)
+ * @param section 페인트를 칠하기로한 구역의 번호
+ */
+function solution(n: number, m: number, section: number[]) {
+  let answer = 0;
+
+  let filledIdx = 0;
+
+  for (const emptyWallPos of section) {
+    if (filledIdx >= emptyWallPos) {
+      continue;
+    }
+    if (emptyWallPos + m - 1 < n + m) {
+      filledIdx = emptyWallPos + m - 1;
+      answer++;
+    }
+    console.log(filledIdx, emptyWallPos);
+  }
+
+  return answer;
+}
+// === 단순 실행 테스트 ===
+// NOSONAR // cspell:disable
+console.log(solution(8, 4, [2, 3, 6]));
+console.log(solution(5, 4, [1, 3]));
+console.log(solution(4, 1, [1, 2, 3, 4]));
+// cspell:enable
+//
+// === 문제 읽고 첫 느낌 ===
+// 어렵다 어려워~~
+// 그런데 잘 생각해보면...
+// 페인트를 칠하는 영역기준으로만 인덱스를 옮겨가면서 진행하면 될 것 같음.
+// 칠하는 영역이 벽의 크기를 넘어가지 않으므로, 2번째에서 넘치것을 생각할 필요가 없고,
+// 넘치거나 같아졌을 때 그냥 끝내면 됨.
+//
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// 1번 풀이를 보니까 더 간단한 것 같다. 😅
+//
+export default solution;
