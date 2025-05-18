012e06bd56f0b870aac468d0397cb1d34b395d39
fp024 <aopenasus@naver.com>
2025-05-18 21:07:19 +0900

    소수 만들기 - 12977

---
+++ src/ts/lv_1/exam048-12977.test.ts
@@ -0,0 +1,20 @@
+import solution from './exam048-12977';
+
+// cspell:disable
+const DEFAULT_TEST_CASES = [
+  {
+    nums: [1, 2, 3, 4],
+    expected: 1,
+  },
+  {
+    nums: [1, 2, 7, 6, 4],
+    expected: 4,
+  },
+];
+// cspell:enable
+
+describe('소수 만들기', () => {
+  test.each(DEFAULT_TEST_CASES)('기본 테스트 케이스 검증', ({ nums, expected }) => {
+    expect(solution(nums)).toBe(expected);
+  });
+});
---
+++ src/ts/lv_1/exam048-12977.ts
@@ -0,0 +1,47 @@
+/**
+ * 소수 만들기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/12977
+ * @param nums   숫자 배열
+ */
+function solution(nums: number[]) {
+  let answer = 0;
+  const N = nums.length;
+
+  for (let i = 0; i < N - 2; i++) {
+    for (let j = i + 1; j < N - 1; j++) {
+      for (let k = j + 1; k < N; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+function isPrime(n: number) {
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR // cspell:disable
+console.log(solution([1, 2, 3, 4]));
+console.log(solution([1, 2, 7, 6, 4]));
+
+// cspell:enable
+//
+// === 문제 읽고 첫 느낌 ===
+//   개별 요소의 소수를 구하는 건 별로 문제는 아닌데,
+//   3중 루프를 사용해서 3개를 골라내는 것만 잘 생각하면 될 것 같다.
+//   전에도 위처럼 3중 루프로 해결하는 문제가 있었다.
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
