a372b324c1780d18cb2ccbcd9377984e5c87c15c
fp024 <aopenasus@naver.com>
2025-05-29 17:30:32 +0900

    바탕화면 정리 - 161990

---
+++ src/ts/lv_1/exam052-161990.test.ts
@@ -0,0 +1,49 @@
+import solution from './exam052-161990';
+
+// cspell:disable
+const DEFAULT_TEST_CASES = [
+  {
+    wallpaper: [
+      '.#...', //
+      '..#..',
+      '...#.',
+    ],
+    expected: [0, 1, 3, 4],
+  },
+  {
+    wallpaper: [
+      '..........', //
+      '.....#....',
+      '......##..',
+      '...##.....',
+      '....#.....',
+    ],
+    expected: [1, 3, 5, 8],
+  },
+  {
+    wallpaper: [
+      '.##...##.', //
+      '#..#.#..#',
+      '#...#...#',
+      '.#.....#.',
+      '..#...#..',
+      '...#.#...',
+      '....#....',
+    ],
+    expected: [0, 0, 7, 9],
+  },
+  {
+    wallpaper: [
+      '..', //
+      '#.',
+    ],
+    expected: [1, 0, 2, 1],
+  },
+];
+// cspell:enable
+
+describe('바탕화면 정리', () => {
+  test.each(DEFAULT_TEST_CASES)('기본 테스트 케이스 검증', ({ wallpaper, expected }) => {
+    expect(solution(wallpaper)).toBe(expected);
+  });
+});
