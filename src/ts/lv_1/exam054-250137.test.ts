f7291bf314e474dbd3b571438a2f1cd02c02a1eb
fp024 <aopenasus@naver.com>
2025-06-02 07:32:46 +0900

    [PCCP 기출문제] 1번 / 붕대 감기 - 250137
    처음에는 1부터 줄줄이 시간을 반복할까하다가..
    공격시간 기준으로 맞춰서 흐르는 시간 계산해서 하긴했는데, 과연 한방에 통과 될까 했는데.. +1점으로 일단 통과! 😅

---
+++ src/ts/lv_1/exam054-250137.test.ts
@@ -0,0 +1,55 @@
+import solution from './exam054-250137';
+
+// cspell:disable
+const DEFAULT_TEST_CASES = [
+  {
+    bandage: [5, 1, 5],
+    health: 30,
+    attacks: [
+      [2, 10],
+      [9, 15],
+      [10, 5],
+      [11, 5],
+    ],
+    expected: 5,
+  },
+  {
+    bandage: [3, 2, 7],
+    health: 20,
+    attacks: [
+      [1, 15],
+      [5, 16],
+      [8, 6],
+    ],
+    expected: -1,
+  },
+  {
+    bandage: [4, 2, 7],
+    health: 20,
+    attacks: [
+      [1, 15],
+      [5, 16],
+      [8, 6],
+    ],
+    expected: -1,
+  },
+  {
+    bandage: [1, 1, 1],
+    health: 5,
+    attacks: [
+      [1, 2],
+      [3, 2],
+    ],
+    expected: 3,
+  },
+];
+// cspell:enable
+
+describe('[PCCP 기출문제] 1번 / 붕대 감기', () => {
+  test.each(DEFAULT_TEST_CASES)(
+    '기본 테스트 케이스 검증',
+    ({ bandage, health, attacks, expected }) => {
+      expect(solution(bandage, health, attacks)).toBe(expected);
+    }
+  );
+});
---
+++ src/ts/lv_1/exam054-250137.ts
@@ -0,0 +1,100 @@
+/**
+ * [PCCP 기출문제] 1번 / 붕대 감기
+ *   https://school.programmers.co.kr/learn/courses/30/lessons/250137
+ */
+function solution(bandage: number[], health: number, attacks: number[][]) {
+  let answer;
+
+  const MAX_HP = health;
+  const [castTime, recoverPerSec, extraRecover] = bandage;
+
+  // 이전 공격시간
+  let prevAttackTime = 0;
+  // 연속 성공 시간
+  let consecutiveSuccessTime = 0;
+
+  for (let [attackTime, damage] of attacks) {
+    // 공격시간 전
+    for (let t = prevAttackTime + 1; t < attackTime; t++) {
+      health = Math.min(health + recoverPerSec, MAX_HP);
+      consecutiveSuccessTime++;
+
+      if (consecutiveSuccessTime === castTime) {
+        health = Math.min(health + extraRecover, MAX_HP);
+        consecutiveSuccessTime = 0;
+      }
+    }
+
+    // 공격시간
+    prevAttackTime = attackTime;
+    health = health - damage;
+    consecutiveSuccessTime = 0;
+
+    if (health <= 0) {
+      break;
+    }
+  }
+
+  answer = health <= 0 ? -1 : health;
+
+  return answer;
+}
+
+// === 단순 실행 테스트 ===
+// NOSONAR // cspell:disable
+console.log(
+  solution(
+    [5, 1, 5], //
+    30,
+    [
+      [2, 10],
+      [9, 15],
+      [10, 5],
+      [11, 5],
+    ]
+  )
+);
+
+console.log(
+  solution(
+    [3, 2, 7], //
+    20,
+    [
+      [1, 15],
+      [5, 16],
+      [8, 6],
+    ]
+  )
+);
+
+console.log(
+  solution(
+    [4, 2, 7], //
+    20,
+    [
+      [1, 15],
+      [5, 16],
+      [8, 6],
+    ]
+  )
+);
+
+console.log(
+  solution(
+    [1, 1, 1], //
+    5,
+    [
+      [1, 2],
+      [3, 2],
+    ]
+  )
+);
+// cspell:enable
+// === 문제 읽고 첫 느낌 ===
+//   처음에는 1부터 줄줄이 시간을 반복할까하다가..
+//   공격시간 기준으로 맞춰서 흐르는 시간 계산해서 하긴했는데.. 기본 테스트는 통과했다. 😅
+//
+// === 다른 사람 풀이 확인 이후 의견 ===
+// ...
+//
+export default solution;
