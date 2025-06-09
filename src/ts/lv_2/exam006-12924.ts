2c70dfebaab6d3139c1dfb474a475ffd1bc85af9
fp024 <aopenasus@naver.com>
2025-06-10 05:50:06 +0900

    숫자의 표현 - 70129 (진행중)
    - 조건을 단순하게 변경
    - 아직 2중 for문은 유지해서
      효율성 테스트 1~6은 실패

--- src/ts/lv_2/exam006-12924.ts
+++ src/ts/lv_2/exam006-12924.ts
@@ -4,36 +4,19 @@
  */
 function solution(n: number) {
   let answer = 0;
-  let halfNPlus2 = Math.trunc(n / 2) + 2;
-  let lastJ = 0;
 
-  if (n == 1 || n == 2) {
-    return 1;
-  }
-
-  for (let i = 1; i <= halfNPlus2; i++) {
-    let total = 0;
-    let debug: number[] = [];
-    for (let j = i; j <= halfNPlus2; j++) {
-      debug.push(j);
-      total += j;
-
-      if (total === n) {
-        console.log(debug);
+  for (let i = 1; i <= n; i++) {
+    let sum = 0;
+    for (let j = i; sum <= n; j++) {
+      sum += j;
+      if (sum === n) {
         answer++;
-        lastJ = j;
-        i = lastJ - 2;
-        break;
-      } else if (total > n) {
         break;
       }
     }
-    if (lastJ == halfNPlus2 - 1) {
-      break;
-    }
   }
 
-  return answer + 1;
+  return answer;
 }
 
 // === 단순 실행 테스트 ===
@@ -41,7 +24,6 @@ function solution(n: number) {
 console.log(solution(15));
 
 console.log(solution(2));
-console.log(solution(1));
 
 // cspell:enable
 // === 문제 읽고 첫 느낌 ===
