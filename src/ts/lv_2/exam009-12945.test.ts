796df5268407db71d90b0c4d79d51104c06a311b
fp024 <aopenasus@naver.com>
2025-06-10 12:59:48 +0900

    재귀 전환과 모듈러 공식에 대해서...
    AI와의 대화내용 추가 😅

--- src/ts/lv_2/exam009-12945.test.ts
+++ src/ts/lv_2/exam009-12945.test.ts
@@ -12,6 +12,14 @@ const DEFAULT_TEST_CASES = [
     expected: 5,
   },
 ];
+
+const EXTRA_TEST_CASES = [
+  {
+    n: 100_000,
+    expected: 1_168_141,
+  },
+];
+
 // cspell:enable
 
 describe('피보나치 수', () => {
@@ -22,6 +30,11 @@ describe('피보나치 수', () => {
 
 describe('피보나치 수 - 재귀를 반복으로 + 나누기 연산을 미리 해줌', () => {
   test.each(DEFAULT_TEST_CASES)('기본 테스트 케이스 검증', ({ n, expected }) => {
+    expect(solution_a(n)).toBe(expected);
+  });
+
+  // 개선된 버전만 100_000 값을 감당이 가능하다.
+  test.each(EXTRA_TEST_CASES)('추가 테스트 케이스 검증', ({ n, expected }) => {
     expect(solution_a(n)).toBe(expected);
   });
 });
