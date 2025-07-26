83976c5448d456d3318dc130571439ed49fdd02a
fp024 <aopenasus@naver.com>
2025-07-26 16:58:06 +0900

    주식가격 - 42584 - O(n^2) 풀이

--- src/ts/lv_2/exam013-42584.ts
+++ src/ts/lv_2/exam013-42584.ts
@@ -6,11 +6,25 @@ import { isStandalone } from '../../utils/testHelper.js';
 
 /*
    문제 풀이 전략
-    1. ...
+    0. 이번에는 단순하게 정확도만 따져보자!
+      - 효율성은 이후 생각해보자!
+    1. 문제 사양
+      - 인덱스가 초(sec)
+      - 값이 주식 시세(원)
+    2. 현재 가격 보다 더 낮은 가격이 되기 바로 직전의 시간(초)
 */
 
 function solution(prices: number[]) {
-  const answer: number[] = [];
+  const answer: number[] = new Array(prices.length).fill(0);
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      answer[i]++;
+      if (prices[i] > prices[j]) {
+        break;
+      }
+    }
+  }
+
   return answer;
 }
 
@@ -21,7 +35,8 @@ if (isStandalone()) {
 }
 // cspell:enable
 // === 문제 읽고 첫 느낌 ===
-// ...
+//   일단은 O(n^2)으로 정확도는 맞는지만 프로그래머스에서 확인했는데,
+//   정확성과 효율성이 통과가 되었음. 😅
 //
 // === 다른 사람 풀이 확인 이후 의견 ===
 // ...
